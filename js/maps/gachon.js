5caa475684e95ab0345bc5fbf8e1a57e34a3b646
Minjae Seon <ddol0225@gmail.com>
2021-11-11 16:44:47 +0900

    Enable Ghost in Gachon Map

--- js/maps/gachon.js
+++ js/maps/gachon.js
@@ -283,7 +283,7 @@ export function initGachonMap(scene, world, controls, camera) {
     Utils.setUserEvent(scene, world, controls, camera);
 
     // 고스트
-    //Utils.createGhost(scene, world, 'ghost1', 0, 360, 0, 0xFFFF00);
+    Utils.createGhost(scene, world, 'ghost1', 0, 360, 0, 0xFFFF00);
 
     // 동글이들
     // 제일 왼쪽 세로줄
